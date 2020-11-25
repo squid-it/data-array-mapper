05a77cdbad3058670db908e604403e39795a67ff
Cecil Zorg <devop@cecilzorg.nl>
2020-11-25 17:54:31 +0100

    fixed indentation

--- tests/Unit/SquidIT/Data/ResultSetToArray/MapperTest.php
+++ tests/Unit/SquidIT/Data/ResultSetToArray/MapperTest.php
@@ -8,136 +8,136 @@ use SquidIT\Data\ResultSetToArray\Mapper;
 
 class MapperTest extends TestCase
 {
-	/**
-	 * @var array $exampleDataSet
-	 */
-	private $exampleDataSet;
+    /**
+     * @var array $exampleDataSet
+     */
+    private $exampleDataSet;
 
     /**
      * @var array $exampleDataSetInvalid
      */
-	private $exampleDataSetInvalid;
+    private $exampleDataSetInvalid;
 
     /**
      * @var array $exampleDataSetUnordered
      */
-	private $exampleDataSetUnordered;
+    private $exampleDataSetUnordered;
 
     /**
      * @var string $dataSetPath
      */
-	private $dataSetPath = __DIR__.'/ResultSet';
+    private $dataSetPath = __DIR__.'/ResultSet';
 
-	/**
-	 * @var array $resultStructureDbColumnToResult
-	 */
-	private $resultStructureDbColumnToResult;
+    /**
+     * @var array $resultStructureDbColumnToResult
+     */
+    private $resultStructureDbColumnToResult;
 
-	/**
-	 * @var array
-	 */
-	private $resultStructureDbColumnToNewColumnResult;
+    /**
+     * @var array
+     */
+    private $resultStructureDbColumnToNewColumnResult;
 
-	/**
-	 * @var array $pivotPoints
-	 */
-	private $pivotPoints;
+    /**
+     * @var array $pivotPoints
+     */
+    private $pivotPoints;
 
-	/**
-	 * setUp
-	 *
-	 * Load some default test values
-	 */
-	public function setUp(): void
-	{
-		// Load example datasets
-		$this->exampleDataSet = require $this->dataSetPath.'/dataset.php';
+    /**
+     * setUp
+     *
+     * Load some default test values
+     */
+    public function setUp(): void
+    {
+        // Load example datasets
+        $this->exampleDataSet = require $this->dataSetPath.'/dataset.php';
         $this->exampleDataSetInvalid = require $this->dataSetPath.'/datasetInvalid.php';
         $this->exampleDataSetUnordered = require $this->dataSetPath.'/datasetUnordered.php';
 
-		// We want out resultSet to follow the below structure
-		$this->resultStructureDbColumnToResult = [
-			'userId',
-			'userName',
-			'age',
-			'toys' => [
-				'toyId',
-				'toyType',
-				'toyName',
-				'placesToyVisited' => [
-					'placeId',
-					'placeName',
-				]
-			]
-		];
+        // We want out resultSet to follow the below structure
+        $this->resultStructureDbColumnToResult = [
+            'userId',
+            'userName',
+            'age',
+            'toys' => [
+                'toyId',
+                'toyType',
+                'toyName',
+                'placesToyVisited' => [
+                    'placeId',
+                    'placeName',
+                ]
+            ]
+        ];
 
-		// We want out resultSet to follow the below structure
-		$this->resultStructureDbColumnToNewColumnResult = [
-			'userId',
-			'userName',
-			'age' => 'leeftijd',
-			'toys' => [
-				'toyId',
-				'toyType' => 'soort',
-				'toyName',
-				'placesToyVisited' => [
-					'placeId',
-					'placeName',
-				]
-			]
-		];
+        // We want out resultSet to follow the below structure
+        $this->resultStructureDbColumnToNewColumnResult = [
+            'userId',
+            'userName',
+            'age' => 'leeftijd',
+            'toys' => [
+                'toyId',
+                'toyType' => 'soort',
+                'toyName',
+                'placesToyVisited' => [
+                    'placeId',
+                    'placeName',
+                ]
+            ]
+        ];
 
-		// We need to define our resultSet pivot points
-		$this->pivotPoints = [
-	  		'[root]' => 'userId',
-	  		'toys' => 'toyId',
-	  		'toys.placesToyVisited' => 'placeId',
-		];
+        // We need to define our resultSet pivot points
+        $this->pivotPoints = [
+            '[root]' => 'userId',
+            'toys' => 'toyId',
+            'toys.placesToyVisited' => 'placeId',
+        ];
 
-	}
+    }
 
-	public function testParseStructureException(): void
-	{
-		$pivotPoints = $this->pivotPoints;
-		unset($pivotPoints['[root]']);
+    public function testParseStructureException(): void
+    {
+        $pivotPoints = $this->pivotPoints;
+        unset($pivotPoints['[root]']);
 
-		$this->expectException(InvalidArgumentException::class);
-		Mapper::parseStructure($this->resultStructureDbColumnToResult, $pivotPoints);
-	}
+        $this->expectException(InvalidArgumentException::class);
+        Mapper::parseStructure($this->resultStructureDbColumnToResult, $pivotPoints);
+    }
 
-	public function testParseStructureColumnToResult(): void
-	{
-		$key = 'placeName';
-		$value = '[userId].toys.[toyId].placesToyVisited.[placeId].placeName';
+    public function testParseStructureColumnToResult(): void
+    {
+        $key = 'placeName';
+        $value = '[userId].toys.[toyId].placesToyVisited.[placeId].placeName';
 
-		$structure = Mapper::parseStructure($this->resultStructureDbColumnToResult, $this->pivotPoints);
-		$this->assertArrayHasKey('userName', $structure);
-		$this->assertArrayHasKey('placeId', $structure);
-		$this->assertEquals($value, $structure[$key]);
-	}
+        $structure = Mapper::parseStructure($this->resultStructureDbColumnToResult, $this->pivotPoints);
+        $this->assertArrayHasKey('userName', $structure);
+        $this->assertArrayHasKey('placeId', $structure);
+        $this->assertEquals($value, $structure[$key]);
+    }
 
-	public function testParseStructureColumnToNewColumn(): void
-	{
-		$key = 'toyType';
-		$value = '[userId].toys.[toyId].soort';
+    public function testParseStructureColumnToNewColumn(): void
+    {
+        $key = 'toyType';
+        $value = '[userId].toys.[toyId].soort';
 
-		$structure = Mapper::parseStructure($this->resultStructureDbColumnToNewColumnResult, $this->pivotPoints);
-		$this->assertEquals($value, $structure[$key]);
-	}
+        $structure = Mapper::parseStructure($this->resultStructureDbColumnToNewColumnResult, $this->pivotPoints);
+        $this->assertEquals($value, $structure[$key]);
+    }
 
-	public function testMapData(): void
-	{
-		$structure = [
-			'userId'	=> '[userId].userId',
-			'placeId'	=> '[userId].toys.[toyId].placesToyVisited.[placeId].placeId',
-		];
+    public function testMapData(): void
+    {
+        $structure = [
+            'userId'	=> '[userId].userId',
+            'placeId'	=> '[userId].toys.[toyId].placesToyVisited.[placeId].placeId',
+        ];
 
-		$resultSet = Mapper::mapData($this->exampleDataSet, $structure);
-		$firstRecord = reset($resultSet);
-		$this->assertArrayHasKey('userId', $firstRecord);
-		$this->assertEquals(3, $firstRecord['userId']);
-		$this->assertEquals(33, $firstRecord['toys'][7]['placesToyVisited'][33]['placeId']);
-	}
+        $resultSet = Mapper::mapData($this->exampleDataSet, $structure);
+        $firstRecord = reset($resultSet);
+        $this->assertArrayHasKey('userId', $firstRecord);
+        $this->assertEquals(3, $firstRecord['userId']);
+        $this->assertEquals(33, $firstRecord['toys'][7]['placesToyVisited'][33]['placeId']);
+    }
 
     public function testMapDataNoPivotKeyIdValue(): void
     {
@@ -186,16 +186,16 @@ class MapperTest extends TestCase
 
     }
 
-	public function testMapDataEmptyDataSet(): void
-	{
-		$structure = [
-			'userId'	=> '[userId].userId',
-			'placeId'	=> '[userId].toys.[toyId].placesToyVisited.[placeId].placeId',
-		];
+    public function testMapDataEmptyDataSet(): void
+    {
+        $structure = [
+            'userId'	=> '[userId].userId',
+            'placeId'	=> '[userId].toys.[toyId].placesToyVisited.[placeId].placeId',
+        ];
 
-		$this->expectException(InvalidArgumentException::class);
-		Mapper::mapData([], $structure);
-	}
+        $this->expectException(InvalidArgumentException::class);
+        Mapper::mapData([], $structure);
+    }
 
     public function testMapDataMissingDataSetColumns(): void
     {
@@ -206,9 +206,9 @@ class MapperTest extends TestCase
         Mapper::mapData($this->exampleDataSetInvalid, $structure);
     }
 
-	public function testMapDataEmptyStructure(): void
-	{
-		$this->expectException(InvalidArgumentException::class);
-		Mapper::mapData($this->exampleDataSet, []);
-	}
+    public function testMapDataEmptyStructure(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Mapper::mapData($this->exampleDataSet, []);
+    }
 }
